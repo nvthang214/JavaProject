7cef719e508e91330f86f29c318c128683f7cc02
nvthang214 <nguyenvanthang520527@gmail.com>
2024-01-21 03:26:30 +0700

    update

--- ConvertNumberSystem/src/Run/Main.java
+++ ConvertNumberSystem/src/Run/Main.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Run;
 
 import Controller.Option;
 import Utils.Validate;
-import java.io.IOException;
-import java.util.Scanner;
 
-/**
- *
- * @author emlad
- */
+
 public class Main {
 
     public static void main(String[] args) {
